0fe638eeeea1508819506e9162026be7a52b3480
Garrick <garrickb@outlook.com>
2013-02-28 21:52:49 -0500

    Framework.

---
+++ src/ImagePuush.java
@@ -0,0 +1,13 @@
+import javax.swing.*;
+
+public class ImagePuush extends Puush {
+
+    public ImagePuush(String name) {
+        super(name);
+    }
+
+    @Override
+    public JPanel fetch() {
+        return null;  //To change body of implemented methods use File | Settings | File Templates.
+    }
+}
